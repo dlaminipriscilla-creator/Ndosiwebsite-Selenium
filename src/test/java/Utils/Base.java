665df0d4b594c7cf529030ffbb883e57108fa59c
Priscilla Matjila <matjilapriscilla@gmail.com>
2026-06-27 14:17:37 +0200

    created base class

---
+++ src/test/java/Utils/Base.java
@@ -0,0 +1,9 @@
+package Utils;
+
+import org.openqa.selenium.WebDriver;
+
+public class Base {
+
+    BrowserFactory browserFactory=new BrowserFactory();
+    public final WebDriver driver = browserFactory.startBrowser("chrome","https://ndosisimplifiedautomation.vercel.app");
+}
